00854a49e8f376697ebc961682123af008d0334c
Osman <91911474+Avarets123@users.noreply.github.com>
2022-04-10 15:38:38 +0300

    Add files via upload

---
+++ Props/offer-list/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import dataBase from './data/data';
+
+import Listing from './Components/Listing';
+
+function App() {
+
+  return <Listing data={dataBase}/>
+
+
+}
+
+export default App;
