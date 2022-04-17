699c8e1486272bff8a1254b391cc117b171c901b
Osman <91911474+Avarets123@users.noreply.github.com>
2022-04-18 02:16:22 +0300

    Add files via upload

---
+++ Composition/decomposition/src/components/WorkError/WorkError.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const WorkError = () => {
+    return (
+        <div className="work">
+            <img src='http://newedu.fizmat.vspu.ru/mabi/files/2014/06/rabota-nad-oshibkami.jpg' alt="curcle" className="curcle" />
+            <a href="##" className="work-error"> Работа над ошибками</a>
+            <div> Смотрите на Яндексе и запоминайте</div>
+        </div>
+
+    );
+}
+
+export default WorkError;
